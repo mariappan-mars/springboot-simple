aa9c878aae03301ecd6bff040d11e83fba22ef1f
Mars <mariappank593@gmail.com>
2022-01-29 20:09:42 +0800

    inital commit

---
+++ src/main/java/com/mars/simpleapp/SimpleappApplication.java
@@ -0,0 +1,20 @@
+package com.mars.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Helloooo";
+	}
+}
